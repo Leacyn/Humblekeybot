c535c2fa309b911a8df144dc0bbc867a4ca371a3
Leacyn <vincent.geers@ziggo.nl>
2018-12-18 16:57:04 +0100

    updating

--- bot.js
+++ bot.js
@@ -2,12 +2,43 @@ const Discord = require('discord.js');
 const Token = require('./auth.json');
 const botconfig = require('./botconfig.json');
 
-
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-bot.on("ready", async () =>{
-  console.log(`${bot.user.username} is online!`)
+fs.readdir("./modules",(err, files) =>{
+	
+	if(err) console.log(err);
+	
+	let jsfile = files.filter(f=> f.split(".").pop() === "js");
+	if (jsfile.length <= 0 ){ 
+		console.log("no modules found");
+		return;	
+	}
+	
+	jsfile.forEach((f,i) =>{
+		let props = require('./modules/${f}').
+		console.log('${f} Loaded');
+		bot.commands.set(props.help.name, props);		
+	});
 });
+bot.on("ready", async () =>{
+  console.log(`${bot.user.username} is online!`);
+  let nbr = (Math.random * 2);
+  switch (nbr) {
+    case 0:
+    console.log()
+      bot.user.setActivity("some sweet OST from mainstream Anime", {type:"LISTENING"});
+      break;
+    case 1:
+      bot.user.setActivity("HuniePop", {type:"PLAYING"});
+      break;
+    default:
+
+	}
+});
+
+
+
 
 
 bot.on("message", async message => {
@@ -32,8 +63,21 @@ bot.on("message", async message => {
         .setDescription("Bot Info")
         .setColor("#487aa0")
         .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username);
+        .addField("Bot Name", bot.user.username)
+        .addField("Created on", bot.user.createdAt);
         return message.channel.send(botembed);
+        break;
+    case `${prefix}serverinfo`:
+        let sicon = message.guild.displayAvatarUrl;
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#ff000f")
+        .addField("Server Name",message.guild.name)
+        .addField("Created on",message.guild.createdAt)
+        .addField("You joined at:", message.member.joinedAt)
+        .addField("Total Members:", message.guild.memberCount);
+        return message.channel.send(serverembed);
+        break;
 
 
     default:
